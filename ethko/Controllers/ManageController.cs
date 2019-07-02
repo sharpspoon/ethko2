ccfae066f22d2798dfb6ae73de5e20e099e21170
Robin Ward <robin@steelcitysites.net>
2019-07-02 11:57:18 -0500

    client billing model updated

--- ethko/Controllers/ManageController.cs
+++ ethko/Controllers/ManageController.cs
@@ -542,7 +542,8 @@ namespace ethko.Controllers
             using (ethko_dbEntities entities = new ethko_dbEntities())
             {
                 var billingMethods = from bm in entities.BillingMethods
-                              select new GetClientBillingViewModel() { BillingMethodId = bm.BillingMethodId.ToString(), BillingMethodName = bm.BillingMethodName, InsDate = bm.InsDate.ToString(), FstUser = bm.FstUser };
+                                     join u in entities.AspNetUsers on bm.FstUser equals u.Id
+                                     select new GetClientBillingViewModel() { BillingMethodId = bm.BillingMethodId.ToString(), BillingMethodName = bm.BillingMethodName, InsDate = bm.InsDate.ToString(), FstUser = u.UserName };
                 return View(billingMethods.ToList());
             }
         }
