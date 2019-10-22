9fc332c36fd0bc458dd28a86ef47ce093761f927
Robin Ward <robin@steelcitysites.net>
2019-10-22 14:49:00 -0500

    more document work, preparing for ocr work

--- ethko/Controllers/DocumentsController.cs
+++ ethko/Controllers/DocumentsController.cs
@@ -14,10 +14,32 @@ namespace ethko.Controllers
             using (ethko_dbEntities entities = new ethko_dbEntities())
             {
                 var documentList = from d in entities.Documents
+                                   join c in entities.Cases on d.CaseId equals c.CaseId
                                     join u in entities.AspNetUsers on d.FstUser equals u.Id
                                     join da in entities.DimDates on d.InsDate equals da.DateKey
-                                    select new GetDocumentsViewModel() { DocumentId = d.DocumentId, DocumentName = d.DocumentName, InsDate = da.FullDateUSA.ToString(), UserId = u.UserName, FullName = u.FName + " " + u.LName };
+                                   where d.DocumentTypeId == 1
+                                    select new GetDocumentsViewModel() { DocumentId = d.DocumentId, DocumentName = d.DocumentName, InsDate = da.FullDateUSA.ToString(), UserId = u.UserName, FullName = u.FName + " " + u.LName, CaseName = c.CaseName };
                 return View(documentList.ToList());
+            }
+        }
+
+        //View Specific Document
+        [HttpGet]
+        public ActionResult ViewDocument(int DocumentId)
+        {
+            using (ethko_dbEntities entities = new ethko_dbEntities())
+            {
+                var document = (from d in entities.Documents
+                                join dd in entities.DimDates on d.InsDate equals dd.DateKey into dateopened
+                                from x in dateopened.DefaultIfEmpty()
+                                where d.DocumentId == DocumentId
+                                select new GetIndividualDocumentViewModel()
+                                {
+                                    DocumentId = d.DocumentId,
+                                    DocumentName = d.DocumentName
+                                }).FirstOrDefault();
+
+                return View(document);
             }
         }
     }
--- ethko/Models/DocumentViewModels.cs
+++ ethko/Models/DocumentViewModels.cs
@@ -29,6 +29,27 @@ namespace ethko.Models
         public string FullName { get; set; }
     }
 
+    public class GetIndividualDocumentViewModel
+    {
+        [Display(Name = "Document Id")]
+        public int DocumentId { get; set; }
+
+        [Display(Name = "Document Name")]
+        public string DocumentName { get; set; }
+
+        [Display(Name = "Case Name")]
+        public string CaseName { get; set; }
+
+        [Display(Name = "UserId")]
+        public string UserId { get; set; }
+
+        [Display(Name = "InsDate")]
+        public string InsDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+    }
+
     public class AddDocumentsViewModel
     {
         [Display(Name = "Document Name")]
