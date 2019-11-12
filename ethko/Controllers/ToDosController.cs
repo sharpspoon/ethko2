5ac17865db8ee892e7b1bb006a43cec75c2c35ff
Robin Ward <robin@steelcitysites.net>
2019-11-12 12:45:51 -0600

    to do work

---
+++ ethko/Controllers/ToDosController.cs
@@ -0,0 +1,54 @@
+﻿using ethko.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ethko.Controllers
+{
+    [Authorize]
+    public class ToDosController : Controller
+    {
+        ethko_dbEntities entities = new ethko_dbEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewToDoModal()
+        {
+            var priorityNames = new SelectList(entities.Priorities.ToList(), "PriorityName", "PriorityName");
+            ViewData["DBPriorityNames"] = priorityNames;
+            return PartialView("_AddToDoModal");
+        }
+
+        public ToDo ConvertViewModelToModel(AddToDoViewModel vm)
+        {
+            return new ToDo()
+            {
+                ToDoName = vm.ToDoName
+            };
+        }
+
+        [HttpPost]
+        public ActionResult NewToDoModal(AddToDoViewModel model)
+        {
+            var todoModel = ConvertViewModelToModel(model);
+            var user = User.Identity.GetUserName().ToString();
+            DateTime date = DateTime.Now;
+            int intDate = int.Parse(date.ToString("yyyyMMdd"));
+
+            using (ethko_dbEntities entities = new ethko_dbEntities())
+            {
+                entities.ToDos.Add(todoModel);
+                todoModel.InsDate = intDate;
+                string priorityName = Request.Form["ContactGroups"].ToString();
+                todoModel.ToDoPriorityId = entities.Priorities.Where(m => m.PriorityName == priorityName).Select(m => m.PriorityId).FirstOrDefault();
+                todoModel.FstUser = entities.AspNetUsers.Where(m => m.Email == user).Select(m => m.Id).First();
+                entities.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ethko/Models/ToDoViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ethko.Models
+{
+    //Add
+    public class AddToDoViewModel
+    {
+        [Required]
+        [Display(Name = "To-Do Name")]
+        public string ToDoName { get; set; }
+    }
+}
